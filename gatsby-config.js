c71ecbc41a76521e255f25ada49df6dcb18cd84c
mattgitsit <39727866+mattgitsit@users.noreply.github.com>
2020-12-22 22:20:57 -0800

    installed gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-sass',
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-datocms`,
